f07aa1f1f27f8afea5d13d806b88bec04568aa39
mandybou <boua1424@usherbrooke.ca>
2024-02-12 17:52:16 -0500

    menuInformation pour avoir les infos du jeu

--- Cardjistu/Cardjistu/Afficher.cpp
+++ Cardjistu/Cardjistu/Afficher.cpp
@@ -25,4 +25,16 @@ void Afficher::menuPrincipal()
 void Afficher::menuInformation()
 {
 	system("cls");
+
+	std::cout << "Les joueurs commencent chacun avec 5 cartes. Chaque carte a un element (Feu, Neige ou Eau), un nombre (entre 1 et 10) et une couleur." << std::endl;
+	std::cout << "Au debut de chaque manche, chaque joueur choisi une carte a jouer." << std::endl;
+	std::cout << "La meilleure carte remporte la manche. Le Feu l'emporte sur la Neige, la Neige l'emporte sur l'Eau, l'Eau l'emporte sur le Feu. Si les deux cartes ont le même élément, le plus haut Nombre l'Emporte." << std::endl;
+	std::cout << "La carte gagante est placee dans la pile gagnate du joueur qui a remporté la manche, et la carte perdante est éliminee." << std::endl;
+	std::cout << "Si l'element et le nombre est le même, aucun des joueurs ne l'emportent et ils conservent leur carte" << std::endl;
+	std::cout << "Pour remporter la partie, le joueur doit avoir un set gagnant dans sa pile, soit : " << std::endl;
+	std::cout << "	- Trois cartes du meme element;" << std::endl;
+	std::cout << "	OU" << std::endl;
+	std::cout << "	- Trois cartes d'un element different, chacune de couleur differente." << std::endl;
+	std::cout << "Pour retourner au menu principal, inserez P" << std::endl;
+	std::cout << "Input : ";
 }
