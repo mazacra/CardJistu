c9882a46615950cdaedf60258b3fe21ca3e54a32
mandybou <boua1424@usherbrooke.ca>
2024-02-13 16:44:15 -0500

    ajout condition si egalite + enlever le else dans winningCard()

--- Cardjistu/Cardjistu/Game.cpp
+++ Cardjistu/Cardjistu/Game.cpp
@@ -33,7 +33,7 @@ void Game::play()
 
 Card* Game::winningCard(Card* c1, Card* c2)
 {
-	//if (c1.getElement() == c2.getElement() && c1.getNumber() == c2.getNumber()) return ???
+	if (c1->getElement() == c2->getElement() && c1->getNumber() == c2->getNumber()) return nullptr;
 
 	if (c1->getElement() == 2 && c2->getElement() == 1) return c1;
 	if (c1->getElement() == 3 && c2->getElement() == 2) return c1;
@@ -43,12 +43,10 @@ Card* Game::winningCard(Card* c1, Card* c2)
 	if (c1->getElement() == 2 && c2->getElement() == 3) return c2;
 	if (c1->getElement() == 3 && c2->getElement() == 1) return c2;
 
-	else
-	{
-		if (c1->getNumber() > c2->getNumber()) return c1;
 
-		if (c1->getNumber() < c2->getNumber()) return c2;
-	}
+	if (c1->getNumber() > c2->getNumber()) return c1;
+
+	if (c1->getNumber() < c2->getNumber()) return c2;
 }
 
 Player Game::winningPlayer()
